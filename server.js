26b62b8c6a21ffe71f9570c8ba4c441b0981916c
Dependency Upgrade Agent <dep-upgrade@example.com>
2025-07-05 23:17:53 +0000

    first add

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.get('/', (req, res) => {
+  res.send('Hello, benchmark!');
+});
+app.listen(3000, () => console.log('Listening on port 3000'));
